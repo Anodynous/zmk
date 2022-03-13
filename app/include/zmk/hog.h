ab2125ae9f6a023e29e4fbc10756d76f88ddf660
Anodynous <anodynestrike@gmail.com>
2022-03-13 21:20:20 +0200

    add zmk_hog_send_mouse_report_direct to header file

--- app/include/zmk/hog.h
+++ app/include/zmk/hog.h
@@ -15,3 +15,6 @@ int zmk_hog_send_keyboard_report(struct zmk_hid_keyboard_report_body *body);
 int zmk_hog_send_consumer_report(struct zmk_hid_consumer_report_body *body);
 int zmk_hog_send_mouse_report(struct zmk_hid_mouse_report_body *body);
 int zmk_hog_send_plover_report(struct zmk_hid_plover_report_body *body);
+
+// Anodynous added below to fix build issue
+int zmk_hog_send_mouse_report_direct(struct zmk_hid_mouse_report_body *body);
